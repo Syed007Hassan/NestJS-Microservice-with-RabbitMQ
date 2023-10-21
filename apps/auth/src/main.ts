11733d78e1004e500acfa08ff14347aba4a81a50
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-10-21 14:02:32 +0500

    code modified

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,26 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
-import { Transport } from '@nestjs/microservices';
+import { NestFactory } from '@nestjs/core';
+
+import { SharedService } from '@app/shared';
+
+import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
   const configService = app.get(ConfigService);
+  const sharedService = app.get(SharedService);
 
-  const USER = configService.get('RABBITMQ_USER');
-  const PASSWORD = configService.get('RABBITMQ_PASS');
-  const HOST = configService.get('RABBITMQ_HOST');
-  const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
+  const queue = configService.get('RABBITMQ_AUTH_QUEUE');
 
-  await app.connectMicroservice({
-    transport: Transport.RMQ,
-    options: {
-      url: `amqp://${USER}:${PASSWORD}@${HOST}`,
-      queue: QUEUE,
-      queueOptions: {
-        durable: false,
-      },
-    },
-  });
+  app.connectMicroservice(sharedService.getRmqOptions(queue));
+  app.startAllMicroservices();
 }
 bootstrap();
